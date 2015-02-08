b9ef31f02113f670ebb16da4af10cdf91863fe82
jkenney9a <jkenney9a@gmail.com>
2015-02-08 14:40:31 -0500

    Updated to allow for either a list in a .txt file or an individual .csv file. Also updated comments to clarify use.

--- Ctrax_zebrafish_tracking.py
+++ Ctrax_zebrafish_tracking.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Code for analyzing zebrafish csv output from Ctrax
+Code for analyzing zebrafish tracking csv output from Ctrax 
+For use from shell/command line
 
 Inputs: 
 Argument 1: File of the list of filenames to be analyzed (no extensions)
-Argument 2: Filename of output list
+            or a single csv file output from Ctrax.
+Argument 2: Filename of output CSV file
 Argument 3: Mode and length (e.g., time=10 or fps=30). 
-            Note: time is in minutes
+            Note: time is in minutes, fps = frames per second of video
+
+Output:
+CSV file listing the percent time fish spends in different parts of tank divided
+by halves and by thirds.
 
 Author: 
 Justin W. Kenney
@@ -34,29 +40,34 @@ def combine_df(df):
     """
     Input: dataframe of tracking data (from CSV file)
 
-    Output: dataframe of x and y coordinates of mouse
+    Output: dataframe of x and y coordinates of fish. Assumes one fish and
+    combines different IDs from ctrax into one track.
     """
     
     coordinates = {'x':[], 'y':[]}
     
     for frame in df.index:
-                
+        
+        #Get x,y coordinates of all ids present in file. The CSV output files
+        #from Ctrax are setup in blocks of 6 columns where the 1st is the ID
+        # and the 2nd and 3rd or the x and y coordinates, respectively.        
         tracker_ids = range(0,len(df.columns),6)
         x_temp = []
         y_temp = []
         
         for ids in tracker_ids:
+            #Ids without tracking info are "-1" in the CSV file under ID
             if df[ids][frame] >= 0:
                 x_temp.append(df[ids + 1][frame])
                 y_temp.append(df[ids + 2][frame])
         
-        #Take the last of the appended coordinates (i.e, last id)
+        #Take the last of the appended coordinates (i.e, last id) and throw out
+        #any spurious detections (i.e., len > 0)
         if len(x_temp) > 0:
             coordinates['x'].append(x_temp[-1])
             coordinates['y'].append(y_temp[-1])
         
     return pd.DataFrame(coordinates, columns = ['x','y'])
-
 
 def time_per_frame(df, trial_length):
     """
@@ -73,7 +84,9 @@ def time_per_frame(df, trial_length):
 def min_by_min_top_bottom_analysis(df, top, bottom, trial, mode="time"):
     """
     Input: dataframe of tracking data, 
-    pixel coordinates for top and bottom of tank and mode (time or fps)
+    top, bottom = coordinates for top and bottom of tank (in same space as tracking data) 
+    trial = the time or fps (frames per second), e.g., 5 or 30
+    mode = "time" or "fps"
     
     Output: df of time spent in various parts of tank broken down by minute
     """
@@ -96,7 +109,7 @@ def min_by_min_top_bottom_analysis(df, top, bottom, trial, mode="time"):
         frame_index = [x*frames_per_min for x in range(0,trial_length + 1)]
         
         for i in range(0,len(frame_index)-1):    
-            #Stop trial at last index; ignores data after specified length
+            #Stop trial at last index;
             frames = range(frame_index[i],frame_index[i+1])        
             Output = {x: 0 for x in Parameters}
            
@@ -133,6 +146,7 @@ def min_by_min_top_bottom_analysis(df, top, bottom, trial, mode="time"):
         for t in range(0,len(time_intervals)-1):
             frames = range(time_intervals[t] * int(fpm), int(time_intervals[t+1] * fpm))
             Output = {x: 0 for x in Parameters}
+            
             for frame in frames:
                 if df['y'][frame] >= half and df['y'][frame] >= two_thirds:
                     Output['top 1/2'] += 1
@@ -152,15 +166,11 @@ def min_by_min_top_bottom_analysis(df, top, bottom, trial, mode="time"):
     
     return df_out
 
-
-
-
-
 def get_top_and_bottom(ann_file):
     """
     Input: Annotation file output from Ctrax
     
-    Output: Top and bottom coordinates for tank as dict
+    Output: Top, bottom, left, right coordinates for tank as dict
     """
     #Pulls out the pickled ROI object, which is a numpy array of the coordinates
     with open(ann_file, 'r') as f:
@@ -175,7 +185,7 @@ def get_top_and_bottom(ann_file):
     #Unpickle the sucker
     roi = pickle.loads(roi_string)
    
-    #Get largest and smallest y coordinates
+    #Get largest and smallest x and y coordinates (left/right/top/bottom of tank)
     output = {}
     output["top"] = max([roi[0][i][1] for i in range(len(roi[0]))])
     output["bottom"] = min([roi[0][i][1] for i in range(len(roi[0]))])
@@ -184,19 +194,28 @@ def get_top_and_bottom(ann_file):
         
     return output
     
+    
 
-def main():
-    f = open(sys.argv[1])
-    files = [filename.strip('\n') for filename in f]
-    f.close()
-    All_output = open(sys.argv[2], 'a')
+def analyze_file(files, file_type, output, mode):
+    """
+    Analyzes files
     
-    mode = sys.argv[3]
+    Input: list of filenames, file_type = .txt or .csv, output file name,
+    mode ("time=xx" or "fps=xx"), 
     
-    #Pull out the mode type from the second argument    
+    Output: None, writes data to the output file
+    """
+    if file_type == ".txt":
+        f = open(sys.argv[1])
+        files = [filename.strip('\n') for filename in f]
+        files = [filename.strip('.csv') for filename in files]
+        f.close()
+    
+    output_file = open(output,'a')
+    
     mode_type = mode[:mode.find('=')].lower()
     
-    if mode_type == 'time':
+    if mode_type == "time":
     
         trial_length = int(mode[mode.find('=')+1:])
         
@@ -204,43 +223,99 @@ def main():
         blanks = trial_length * " "
         blank_line = pd.DataFrame(blanks, index = [1], columns = range(trial_length))
         
-        for filename in files:
-            df = load_data(filename + ".csv")
-            df = combine_df(df)
-            t_b = get_top_and_bottom(filename + ".avi.ann")
-            df_out = min_by_min_top_bottom_analysis(df, t_b['top'], t_b['bottom'], 
-                                                    trial_length)
-            df_out.to_csv(All_output, index_label=filename)
-            blank_line.to_csv(All_output, index=False, header=False)
-            print filename + " is done!"
-    
-    elif mode_type == 'fps':
+        if file_type == ".txt":
+            for filename in files:
+                df = load_data(filename + ".csv")
+                df = combine_df(df)
+                t_b = get_top_and_bottom(filename + ".avi.ann")
+                df_out = min_by_min_top_bottom_analysis(df, t_b['top'], t_b['bottom'], 
+                                                        trial_length)
+                df_out.to_csv(output_file, index_label=filename)
+                blank_line.to_csv(output_file, index=False, header=False)
+                print filename + " is done!"
         
-        for filename in files:
-            df = load_data(filename + ".csv")
+        elif file_type == '.csv':
+            df = load_data(files)
+            df = combine_df(df)
+            ann_file = files.strip('.csv') + '.avi.ann'
+            t_b = get_top_and_bottom(ann_file)
+            df_out = min_by_min_top_bottom_analysis(df, t_b['top'], t_b['bottom'], 
+                                                        trial_length)
+            df_out.to_csv(output_file, index_label=files)
+            blank_line.to_csv(output_file, index=False, header=False)
+            print files + " is done!"
+            
+    
+    elif mode_type == "fps":
+        
+        if file_type == ".txt":
+            for filename in files:
+                df = load_data(filename + ".csv")
+                df = combine_df(df)
+                fps = float(mode[mode.find('=')+1:])
+                fpm = fps*60 #Calculate frames per minute
+                trial_length = len(df.index)/float(fpm)
+                
+                #Add blank lines to output CSV to make it look pretty
+                #Need to put it in the loop this time b/c of variable video lengths 
+                #using fps mode
+                blanks = int(trial_length + 1) * " "
+                blank_line = pd.DataFrame(blanks, index = [1], 
+                                          columns = range(int(trial_length)))
+                
+                t_b = get_top_and_bottom(filename + ".avi.ann")
+                
+                df_out = min_by_min_top_bottom_analysis(df, t_b['top'], t_b['bottom'],
+                                                        fps, mode=mode_type)
+                df_out.to_csv(output_file, index_label=filename)
+                blank_line.to_csv(output_file, index=False, header=False)
+                print filename + " is done!"
+            
+        elif file_type == ".csv":
+            df = load_data(files)
             df = combine_df(df)
             fps = float(mode[mode.find('=')+1:])
-            fpm = fps*60
+            fpm = fps*60 #Calculate frames per minute
             trial_length = len(df.index)/float(fpm)
             
             #Add blank lines to output CSV to make it look pretty
-            #Need to put it in the loop this time b/c of variable video lengths 
-            #using fps mode
             blanks = int(trial_length + 1) * " "
             blank_line = pd.DataFrame(blanks, index = [1], 
                                       columns = range(int(trial_length)))
             
-            t_b = get_top_and_bottom(filename + ".avi.ann")
+            ann_file = files.strip('.csv') + '.avi.ann'
+            t_b = get_top_and_bottom(ann_file)
             
             df_out = min_by_min_top_bottom_analysis(df, t_b['top'], t_b['bottom'],
                                                     fps, mode=mode_type)
-            df_out.to_csv(All_output, index_label=filename)
-            blank_line.to_csv(All_output, index=False, header=False)
-            print filename + " is done!"
+            df_out.to_csv(output_file, index_label=files)
+            blank_line.to_csv(output_file, index=False, header=False)
+            print files + " is done!"
+            
+            
+    output_file.close()
     
-    All_output.close()
+
+def main():
     
-#main()
+    files = sys.argv[1]    
+    output = sys.argv[2]
+    mode = sys.argv[3]
+    
+    file_type = files[files.find('.'):].lower()
+    
+    if file_type == ".txt" or file_type == ".csv":
+        analyze_file(files, file_type, output, mode)
+    
+    else:
+        print "Not a supported file type."
+        print "File must be a list of filenames in a .txt file or .csv output"
+        print "from Ctrax."
+    
+    
+        
+#Run the program
+main()
 
     
     
